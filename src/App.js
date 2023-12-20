c6c90e54166edfee693f23c280571995847ba332
Dio <47830594+locke613@users.noreply.github.com>
2023-12-19 23:29:57 -0800

    Initial impl of Ada For All

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Header from './Header';
 
-function App() {
+const App = () => {
+  const [worldPopulation, setWorldPopulation] = useState(null);
+  const [adaPrice, setAdaPrice] = useState(null);
+  const [adaTotalSupply, setAdaTotalSupply] = useState(null);
+  const [adaToPopulationRatio, setAdaToPopulationRatio] = useState(null);
+  const textStyle = {
+    fontSize: '16px',
+    lineHeight: '34px',
+    fontFamily: 'Montserrat',
+  };
+
+  useEffect(() => {
+    const fetchWorldPopulation = async () => {
+      try {
+        const response = await fetch('https://restcountries.com/v3.1/all');
+        if (response.ok) {
+          const data = await response.json();
+          const totalPopulation = data.reduce((acc, country) => acc + country.population, 0);
+          setWorldPopulation(totalPopulation);
+        } else {
+          console.error('Failed to fetch world population');
+        }
+      } catch (error) {
+        console.error('Error fetching world population:', error);
+      }
+    };
+
+    const fetchAdaPrice = async () => {
+      try {
+        const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=cardano&vs_currencies=usd');
+        if (response.ok) {
+          const data = await response.json();
+          setAdaPrice(data.cardano.usd);
+        } else {
+          console.error('Failed to fetch ADA price');
+        }
+      } catch (error) {
+        console.error('Error fetching ADA price:', error);
+      }
+    };
+
+    // Fetch ADA (Cardano) total supply with project_id
+    const fetchAdaSupply = async () => {
+      try {
+        const response = await fetch('https://cardano-mainnet.blockfrost.io/api/v0/network', {
+          headers: {
+            'project_id': 'mainnetS7iOmqEbA6VAEXxwXyKro3vebHjuU7zn'
+          }
+        });
+        if (response.ok) {
+          const data = await response.json();
+          setAdaTotalSupply(data.supply.total/1000000);
+        } else {
+          console.error('Failed to fetch ADA total supply');
+        }
+      } catch (error) {
+        console.error('Error fetching ADA total supply:', error);
+      }
+    };
+
+    fetchWorldPopulation();
+    fetchAdaPrice();
+    fetchAdaSupply();
+  }, []);
+
+  useEffect(() => {
+    if (adaTotalSupply !== null && worldPopulation !== null) {
+      const ratio = (adaTotalSupply / worldPopulation).toFixed(9); // Adjust decimal places as needed
+      setAdaToPopulationRatio(ratio);
+    }
+  }, [adaTotalSupply, worldPopulation]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <div style={{ backgroundColor: 'white', color: 'black', textAlign: 'center', padding: '20px' }}>
+        <div>
+          <h2 style={{ textStyle }}>ADA to World Population Ratio</h2>
+          {adaToPopulationRatio !== null ? <p>{adaToPopulationRatio} ADA per person</p> : <p>Loading...</p>}
+        </div>
+        <div>
+          <h2 style={{ textStyle }}>Cardano (ADA) Price (USD)</h2>
+          {adaPrice !== null ? <p>${adaPrice}</p> : <p>Loading...</p>}
+        </div>
+        <div >
+          <h2 style={{ textStyle }}>Cardano (ADA) Total Supply</h2>
+          {adaTotalSupply !== null ? <p>{adaTotalSupply} ADA</p> : <p>Loading...</p>}
+        </div>
+        <div>
+          <h2 style={{ textStyle }}>World Population</h2>
+          {worldPopulation !== null ? <p>{worldPopulation}</p> : <p>Loading...</p>}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CardanoLogo from './cardano.png'; 
+
+const Header = () => {
+  return (
+    <div style={{ backgroundColor: 'black', color: 'white', padding: '10px', textAlign: 'center' }}>
+        <img src={CardanoLogo} alt="Cardano Logo" style={{ width: '50px', marginRight: '10px' }} />
+    </div>
+  );
+};
+
+export default Header;
